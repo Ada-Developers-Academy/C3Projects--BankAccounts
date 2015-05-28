1bae660adf052956055ddef99b8e8ca7779e5ece
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-05-28 16:24:32 -0700

    Accounts: Added some rspecs, wrote initialize method.

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -2,6 +2,13 @@ module BankAccounts
 
   class Account
 
+    attr_accessor :id, :initial_balance
+
+    def initialize(id, initial_balance)
+      @id = id
+      @initial_balance = initial_balance
+    end
+
   end
 
 end
--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -1,1 +1,23 @@
 require 'spec_helper'
+
+describe BankAccounts::Account do
+
+  it "responds to #new" do
+    expect(BankAccounts::Account).to respond_to(:new)
+  end
+
+  it "responds to #new with two arguments" do
+    expect(BankAccounts::Account).to respond_to(:new).with(2).arguments
+  end
+
+  let(:bob) { BankAccounts::Account.new(1, 1000) }
+
+  it "contains @id" do
+    expect(bob.id).to eq(1)
+  end
+
+  it "contains @initial_balance" do
+    expect(bob.initial_balance).to eq(1000)
+  end
+
+end
