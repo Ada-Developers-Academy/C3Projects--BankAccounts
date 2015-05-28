27d704c710b89751e8260860633a471ed52ba839
Kari <kari.lonczak@gmail.com>
2015-05-28 16:41:39 -0700

    Created self.new class method for Acount.

--- lib/bankaccount/account.rb
+++ lib/bankaccount/account.rb
@@ -2,8 +2,15 @@ module BankAccount
 
 	class Account
 
-		def self.new
+		attr_accessor :id, :initial_balance
+
+		def initialize(id, initial_balance)
+			raise ArgumentError.new "Please enter a positive initial balance." unless initial_balance >= 0
+				@id = id
+				@initial_balance = initial_balance
 		end
+
+
 
 	end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,5 +7,21 @@ describe BankAccount::Account do
 		expect(BankAccount::Account).to respond_to :new
 	end
 
+	context "it creates an instance of a new Account with an id and a positive initial_balance" do
+		let(:account) { BankAccount::Account.new(1, 500) }
+
+		it "has the id 1" do
+			expect(account.id).to eq 1
+		end
+
+		it "has an initial_balance of 500" do
+			expect(account.initial_balance).to eq 500
+		end
+
+		it "raises an error if a negative initial_balance is entered" do
+			expect{BankAccount::Account.new(4, -400)}.to raise_exception(ArgumentError)
+		end
+	end
+
 
 end # describe
