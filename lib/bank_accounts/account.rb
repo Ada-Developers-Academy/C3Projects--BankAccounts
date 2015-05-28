da1bcc4f477742d094181499e5a6b56376dae5cc
Anita <asovay@gmail.com>
2015-05-28 16:06:56 -0700

    Added method to wrong class file. Oops.

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -1,5 +1,10 @@
 module BankAccounts
   class Account
+
+    def initialize(id, initial_balance)
+      @id = id
+      @initial_balance = initial_balance
+    end
    # self.new(id, initial_balance): creates a new instance with the instance variable id and 'initial_balance' assigned
    # Account cannot be created with initialize negative balance
   end # class
--- lib/bank_accounts/savings.rb
+++ lib/bank_accounts/savings.rb
@@ -1,9 +1,7 @@
 module BankAccounts
   class Savings
 
-    def initialize(id, initial_balance)
-      
-    end
+
 
   end # class
 end # module
