701eea90e021bf9ef350880ee0f038e112da3695
Anita <asovay@gmail.com>
2015-05-28 16:12:24 -0700

    Updated README

--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'bank_accounts'
 
 describe BankAccounts::Account do
-  it "responds to class instantiation" do
+  it "responds to class instantiation with 2 arguments" do
     expect(BankAccounts::Account).to respond_to(:new).with(2).arguments
   end
 
