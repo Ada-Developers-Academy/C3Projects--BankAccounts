91f0e8b24f785fbbf283ee4af6f6fc7265d81bd0
Shanna Chau <shannachau@gmail.com>
2015-05-29 11:05:20 -0700

    Added error message methods

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -4,27 +4,41 @@ module BankAccounts
   class Account
     attr_accessor :id, :balance
 
+    def negative_num_error
+      raise ArgumentError.new("Cannot pass in negative argument")
+    end
+
+    def not_num_error
+      raise ArgumentError.new("Parameter must be a number")
+    end
+
+    def balance_error
+      raise ArgumentError.new("Balance needs to be greater than $0")
+    end
+
 
     def initialize(id, initial_balance)
-      raise ArgumentError.new("Cannot have negative number for initial balance") if initial_balance < 0
+      not_num_error unless id.class == Fixnum && initial_balance.class == Fixnum
+
+      balance_error unless initial_balance > 0
 
       @id = id
       @balance = initial_balance
     end
 
 
-    def withdrawl(amount)
+    def withdraw(amount)
       if amount > @balance
-        puts ("Insufficient funds. Please withdrawl an amount less than or equal to #{@balance}")
+        puts ("Insufficient funds. Please withdraw an amount less than or equal to #{@balance}")
+        return @balance
       else
         @balance -= amount
       end
 
-      return @balance
     end
 
     def deposit(amount)
-      raise ArgumentError.new("Cannot have negative number for initial balance") if amount < 0
+      negative_num_error if amount < 0
 
       @balance += amount
     end
