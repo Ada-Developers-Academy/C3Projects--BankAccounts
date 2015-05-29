e451544e7366b6260fc2285432a247fb928a1145
Shanna Chau <shannachau@gmail.com>
2015-05-29 10:37:03 -0700

    Added methods for initialize, withdraw, and add_interest

--- lib/bank_accounts/savings_account.rb
+++ lib/bank_accounts/savings_account.rb
@@ -1,5 +1,27 @@
 module BankAccounts
-  class SavingsAccount
+  class SavingsAccount < Account
+    attr_accessor :id, :balance
+
+    def initialize(id, initial_balance)
+      raise ArgumentError.new("Must have minimum balance of $10") if initial_balance < 10
+      super
+    end
+
+    def withdraw(amount)
+      if (@balance - amount - 2) < 10
+        puts "Insufficient funds. Balance cannot be below $10."
+        return @balance
+      else
+        @balance -= (amount + 2)
+      end
+    end
+
+    def add_interest(rate)
+      interest_on_balance = @balance * rate/100
+      @balance += interest_on_balance
+
+      return interest_on_balance
+    end
 
   end
 end
