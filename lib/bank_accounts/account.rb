0c97e72a668b68babd84b40882d889183a49ff9a
Jeri <drvonnjerryxlii@gmail.com>
2015-05-28 18:42:58 -0700

    finished Account class (breakfast)

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -11,16 +11,84 @@ module BankAccounts
   class Account
 
 
-    attr_reader :balance
+    attr_reader :balance, :id
 
 
     ##--------------------------------------------------------------------------
     # INSTANCE METHODS
 
-    def initialize
+    def initialize(id, initial_balance)
+      if initial_balance > 0
+        @balance = initial_balance.round(2)
+      else
+        raise ArgumentError.new("You cannot create an account with a negative balance.")
+      end
+
+      @id = id
     end
 
 
+    def withdraw(amount)
+      # withdrawals can only be numberic values.
+      # dogs, arrays of dogs, hashes of dogs, etc not accepted here.
+      unless validate_number(amount)
+        return false
+      end
 
+      # raises an error if withdrawing negative or no funds.
+      if (amount <= 0)
+        raise ArgumentError.new("You cannot withdraw negative funds! That sounds like it should be a deposit.")
+      end
+
+      # raises an error if withdrawing funds would create a negative balance.
+      if (@balance - amount) < 0
+        raise ArgumentError.new("You cannot withdraw that much. Your balance would be negative.")
+      end
+
+      # adjusts balance.
+      update_balance(-amount)
+
+      return true # since transaction happened.
+    end
+
+
+    def deposit(amount)
+      # deposits can only be numberic values.
+      # cats, arrays of cats, hashes of cats, etc not accepted here.
+      unless validate_number(amount)
+        return false
+      end
+
+      # raises an error if depositing negative funds would create a negative balance.
+      if (amount <= 0)
+        raise ArgumentError.new("You cannot deposit negative funds! That sounds like it should be a withdrawal.")
+      end
+
+      # adjusts balance.
+      update_balance(amount)
+
+      return true # since transaction happened.
+    end
+
+
+    private
+    ##--------------------------------------------------------------------------
+    # PRIVATE METHODS
+
+
+    def update_balance(amount)
+      @balance += amount.round(2)
+
+      return true
+    end
+
+
+    def validate_number(input)
+      unless (input.class == Fixnum) || (input.class == Float)
+        raise ArgumentError.new("That is not a number!")
+      end
+
+      return true
+    end
   end
 end
--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -1,5 +1,84 @@
 describe "BankAccounts::Account" do
-  it "an instance of Account responds to #balance" do
-    expect(BankAccounts::Account.new).to respond_to :balance
+  before :each do
+    id = "Carl"
+    initial_balance = 50.to_f
+    @account = BankAccounts::Account.new(id, initial_balance)
+  end
+
+
+  it "can have instances of the Account class" do
+    expect(@account).to be_an_instance_of(BankAccounts::Account)
+  end
+
+  it "has an initial balance" do
+    expect(@account.balance).to eq(50)
+  end
+
+  it "has a balance in dollars and cents" do
+    expect(@account.balance.class).to eq(Float)
+
+    @account.deposit(0.011)
+    expect(@account.balance).to eq(@account.balance.round(2))
+  end
+
+  it "cannot have a negative initial balance" do
+    expect{ BankAccounts::Account.new("George", -1) }.to raise_error(ArgumentError)
+  end
+
+  context "has a #deposit method" do
+    it "truly" do
+      expect(@account).to respond_to :deposit
+    end
+
+    it "adds funds to the balance and then returns true" do
+      expect(@account.deposit(45)).to eq(true)
+      expect(@account.balance).to eq(95)
+    end
+
+    it "cannot deposit negative or zero funds" do
+      expect{ @account.deposit(-1) }.to raise_error(ArgumentError)
+      expect(@account.balance).to eq(50)
+
+      expect{ @account.deposit(0) }.to raise_error(ArgumentError)
+      expect(@account.balance).to eq(50)
+    end
+
+    it "only accepts numeric deposits" do
+      expect{ @account.deposit("kittens") }.to raise_error(ArgumentError)
+      expect{ @account.deposit("65") }.to raise_error(ArgumentError)
+      expect{ @account.deposit([65, 12.75, 80]) }.to raise_error(ArgumentError)
+      expect{ @account.deposit({deposit: 19.99}) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context "has a #withdraw method" do
+    it "truly" do
+      expect(@account).to respond_to :withdraw
+    end
+
+    it "removes funds from the balance and then returns true" do
+      expect(@account.withdraw(45)).to eq(true)
+      expect(@account.balance).to eq(5)
+    end
+
+    it "cannot withdraw into the negative" do
+      expect{ @account.withdraw(51) }.to raise_error(ArgumentError)
+      expect(@account.balance).to eq(50)
+    end
+
+    it "cannot withdraw negative or zero funds" do
+      expect{ @account.withdraw(-1) }.to raise_error(ArgumentError)
+      expect(@account.balance).to eq(50)
+
+      expect{ @account.withdraw(0) }.to raise_error(ArgumentError)
+      expect(@account.balance).to eq(50)
+    end
+
+    it "only accepts numeric withdrawals" do
+      expect{ @account.withdraw("puppies") }.to raise_error(ArgumentError)
+      expect{ @account.withdraw("45") }.to raise_error(ArgumentError)
+      expect{ @account.withdraw([45, 6.75, 38]) }.to raise_error(ArgumentError)
+      expect{ @account.withdraw({withdrawal: 43.99}) }.to raise_error(ArgumentError)
+    end
   end
 end
