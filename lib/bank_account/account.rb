83d485d8ec696f051310eaeec7c9cdcafea63bab
Loraine <loraine@lorainekv.com>
2015-05-28 17:00:21 -0700

    Got the raise to work.

--- lib/bank_account/account.rb
+++ lib/bank_account/account.rb
@@ -8,6 +8,12 @@ module BankAccount
     def initialize(id, initial_balance)
       @id = id
       @initial_balance = initial_balance
+
+      if @initial_balance < 0
+        raise ArgumentError("That's a negative balance!")
+      end
+
     end
+
   end
 end
