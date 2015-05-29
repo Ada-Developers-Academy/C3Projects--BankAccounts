80342864432d7dcfcf1e7d2556ab0ce7dc1c7205
Loraine <loraine@lorainekv.com>
2015-05-29 11:40:38 -0700

    Got withdraw, deposit, and balance methods working.

--- lib/bank_account/account.rb
+++ lib/bank_account/account.rb
@@ -8,12 +8,32 @@ module BankAccount
     def initialize(id, initial_balance)
       @id = id
       @initial_balance = initial_balance
+      @current_balance = initial_balance
 
       if @initial_balance < 0
-        raise ArgumentError("That's a negative balance!")
+        raise ArgumentError, "You can't have negative monies!"
       end
 
     end
 
-  end
-end
+    def withdraw(need_monies)
+
+      if need_monies <= @current_balance
+        @current_balance -= need_monies
+        @current_balance
+      else
+        return "Sorry, you don't have that much! Withdrawal canceled. Let's keep your current balance at #{@current_balance}."
+      end #conditional
+    end # withdraw method
+
+    def deposit(put_monies)
+      @current_balance += put_monies
+      @current_balance
+    end
+
+    def balance
+      @current_balance
+    end
+
+  end # class
+end # module
--- spec/lib/bank_account/account_spec.rb
+++ spec/lib/bank_account/account_spec.rb
@@ -4,28 +4,43 @@ require 'pry'
 
 describe BankAccount::Account do
 
-  context "Testing out my new method" do
+  context "Instantiating a new account" do
     # Thanks Chef!
     # This 'let' statement allows you to create a variable called 'new account'
     # that runs something. This variable only works for this one context.
+
     let (:new_account) {BankAccount::Account.new(1,200)}
+    let (:sad_account) {BankAccount::Account.new(2,-1)}
 
     it "responds to class method called .new" do
       expect(BankAccount::Account).to respond_to :new
     end
 
-
-    it "ensures that the id instance variable works" do
-      expect(new_account.id).to eq(1)
-    end
-
-    it "ensures that the initial_balance instance variable works" do
-      expect(new_account.initial_balance).to eq(200)
+    it "throws an error when you open an account with a negative balance" do
+      expect{sad_account}.to raise_error(ArgumentError)
     end
 
   end
 
+  context "ATM transactions" do
 
+    let (:new_account) {BankAccount::Account.new(1,50)}
 
-puts "blah"
+    it "lets you withdraw moneh and returns new balance" do
+      expect(new_account.withdraw(5)).to eq(45)
+    end
+
+    it "won't let you overdraft" do
+      expect(new_account.withdraw(51)).to eq("Sorry, you don't have that much! Withdrawal canceled. Let's keep your current balance at 50.")
+    end
+
+    it "lets you to deposit moneh" do
+      expect(new_account.deposit(5)).to eq(55)
+    end
+
+    it "lets you check current balance" do
+      expect(new_account.balance.to eq(50))
+    end
+
+  end
 end
