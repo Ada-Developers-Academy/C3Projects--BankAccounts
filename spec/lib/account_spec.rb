3edeb72926755b47c80e465c7c3ce73a5ad62073
Ashley Watkins <AWatkins31@gmail.com>
2015-05-29 11:49:33 -0700

    Adds minor cleanup

--- spec/lib/account_spec.rb
+++ spec/lib/account_spec.rb
@@ -6,19 +6,19 @@ describe BankAccount::Account do
     end
 
     context 'when instantiating an accepted account' do
-      before :each do
+      before :all do
         @my_account = BankAccount::Account.new(1,125)
       end
 
-      it 'creates an instance' do
+      it 'is part of Account class' do
         expect(@my_account).to be_a BankAccount::Account
       end
 
-      it 'instance has an id' do
+      it 'has an id' do
         expect(@my_account.id).to eq(1)
       end
 
-      it 'instance has a balance' do
+      it 'has a balance' do
         expect(@my_account.balance).to eq(125)
       end
     end
@@ -51,13 +51,20 @@ describe BankAccount::Account do
     end
 
     it 'returns updated account balance' do
-      my_account = BankAccount::Account.new(1,125)
       expect(@my_account.withdraw(25)).to eq(100)
+    end
+
+    it 'handles floats appropriately' do
+      expect(@my_account.withdraw(24.5)).to eq(100.5)
     end
 
     context 'when attempting to overdraw' do
       it 'returns unmodified account balance' do
-        expect(@my_account.withdraw(140)).to eq(125)
+        expect(@my_account.withdraw(126)).to eq(125)
+      end
+
+      it 'handles floats appropriately' do
+        expect(@my_account.withdraw(125.01)).to eq(125)
       end
     end
   end
@@ -77,7 +84,6 @@ describe BankAccount::Account do
     end
 
     it 'returns updated account balance' do
-      my_account = BankAccount::Account.new(1,125)
       expect(@my_account.deposit(25)).to eq(150)
     end
   end
