3b13386a63a22760339fb2d677a57a264a96e0b7
Kyna <khn710@gmail.com>
2015-05-29 15:31:14 -0700

    added specs for check withdrawal

--- spec/lib/bank_account/checking_spec.rb
+++ spec/lib/bank_account/checking_spec.rb
@@ -16,17 +16,26 @@ describe Bank::CheckingAccount do
       expect(checkings.balance).to eq(100)
     end
 
-    it "returns 49 for a withdrawal of 50" do
+    it "returns 49 for ATM withdrawal of 50" do
       expect(checkings.withdraw(50)).to eq(49)
     end
 
+    it "returns 75 for check withdrawal of 25" do
+      expect(checkings.withdraw_using_check(25)).to eq(75)
+      expect(checkings.checks_used).to eq(1)
+    end
+
     context "low balance errors" do
-      it "raises 'Insufficient Funds' for negative balance" do
+      it "raises 'Insufficient Funds' for negative intial balance" do
         expect{Bank::CheckingAccount.new("broke_joke", -10)}.to raise_error(ArgumentError)
       end
 
-      it "returns original balance of 100 for overdraft" do
+      it "returns balance of 100 for ATM overdraft of 1000" do
         expect(checkings.withdraw(1000)).to eq(100)
+      end
+
+      it "returns balance of 100 for check withdrawal of 500" do
+        expect(checkings.withdraw_using_check(500)).to eq(100)
       end
     end
   end
