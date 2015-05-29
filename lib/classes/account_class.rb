c2fd600a11c0c778a5ea0da60bdfca7d815d2a5a
Elia <elgrenier@gmail.com>
2015-05-29 10:17:19 -0700

    Working methods and green specs for Account class

--- lib/classes/account_class.rb
+++ lib/classes/account_class.rb
@@ -1,20 +1,30 @@
 module BankAccount
 
   class Account
-    attr_accessor :id, :initial_balance
+    attr_accessor :id, :initial_balance, :current_balance
 
     def initialize(id, initial_balance)
-      raise ArgumentError.new "You cannot create an account with a negative balance" unless initial_balance >= 0
+      raise ArgumentError.new("You cannot create an account with a negative balance") unless initial_balance > 0
         @id = id
         @initial_balance = initial_balance
+        @current_balance = initial_balance
     end
 
-    # def negativebalance
-    #   if @initial_balance < 0
-    #   end
-    # end
+    def withdraw(amount)
+      if amount > @current_balance
+        puts "#{amount} is too large of of a withdrawal. Your current balance is #{@current_balance}."
+        return @current_balance
+      else
+        @current_balance = @current_balance - amount
+      end
+    end
 
-    def withdraw
+    def deposit(amount)
+      @current_balance = @current_balance + amount
+    end
+
+    def balance
+      return @current_balance
     end
 
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,43 +4,45 @@ require 'bankaccounts'
 describe BankAccount::Account do
 
   describe "It is initialized with assignments" do
-  let (:bankaccount) {BankAccount::Account.new(1, 10_000)}
+  let (:bankaccount) {BankAccount::Account.new(1, 10000)}
 
     it "has an ID of 1" do
       expect(bankaccount.id).to eq(1)
     end
 
-    it "has an initial balance of 10_000" do
+    it "has an initial balance of 10000" do
       expect(bankaccount.initial_balance).to eq(10000)
     end
 
-    it "cannot have a negative account value initialized" do
+    it "raises an ArgumentError if a negative account value is initialized" do
       expect{BankAccount::Account.new(3, -5000)}.to raise_error(ArgumentError)
+    end
   end
 
-  describe "It has instance methods" do
-  let (:bankaccount) {BankAccount::Account.new(2, 100_000)}
+  describe "It has instance methods with functionality" do
+  let (:bankaccount) {BankAccount::Account.new(2, 10000)}
 
     it "Has a withdraw method" do
       expect(bankaccount).to respond_to :withdraw
     end
 
+    it "Returns a balance of 5000 when 5000 is withdrawn" do
+      expect(bankaccount.withdraw(5000)).to eq(5000)
+    end
+
+    it "Returns a warning and original balance if withdrawal is greater than balance amount" do
+      expect(bankaccount.withdraw(20000)).to eq(10000)
+    end
+
+    it "Returns a balance of 15000 when 5000 is deposited" do
+      expect(bankaccount.deposit(5000)).to eq(15000)
+    end
+
+    it "Balance method shows current balance" do
+      expect(bankaccount.balance).to eq(10000)
+    end
+
   end
 
 
-
-    # describe "attributes" do
-    # let(:sale) { FarMar::Sale.find(1) }
-    # # 1,People's Co-op Farmers FarMar::Sale,30,Portland,Multnomah,Oregon,97202
-    #
-    # it "has the id 1" do
-    #   expect(sale.id).to eq 1
-    # end
-    #
-    # it "has the amount in cents 9290" do
-    #   expect(sale.amount).to eq 9290
-    # end
-
-
-  end
 end
