eb288a174b81f06cae433b727bdc3ca883fabb0f
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-05-29 10:31:43 -0700

    Adding withdraw rspecs & method.

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -2,11 +2,17 @@ module BankAccounts
 
   class Account
 
-    attr_accessor :id, :initial_balance
+    attr_accessor :id, :balance
 
     def initialize(id, initial_balance)
+      raise ArgumentError, "negative number" if initial_balance < 0
+
       @id = id
-      @initial_balance = initial_balance
+      @balance = initial_balance
+    end
+
+    def withdraw
+
     end
 
   end
--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 describe BankAccounts::Account do
 
@@ -16,8 +17,20 @@ describe BankAccounts::Account do
     expect(bob.id).to eq(1)
   end
 
-  it "contains @initial_balance" do
-    expect(bob.initial_balance).to eq(1000)
+  it "contains @balance" do
+    expect(bob.balance).to eq(1000)
+  end
+
+  it "cannot initialize with a negative initial_balance" do
+    expect{BankAccounts::Account.new(2, -1000)}.to raise_error(ArgumentError)
+  end   # Why does raise_error use expect{} vs expect()???
+
+  it "responds to #withdraw" do
+    expect(bob).to respond_to(:withdraw)
+  end
+
+  it "#withdraw subtracts & returns 'amount' from the balance" do
+    expect(bob.withdraw(100)).to eq(900)
   end
 
 end
