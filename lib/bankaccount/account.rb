96c965e9c369ff51936c49cd59966c47d7a9c28c
Ashley Watkins <AWatkins31@gmail.com>
2015-05-28 18:58:24 -0700

    Adds Account#balance, Account.new(id, initial_balance), and associated 6 passing specs.
    Additional 8 specs written for remaining 2 Account breakfast methods.

--- lib/bankaccount/account.rb
+++ lib/bankaccount/account.rb
@@ -1,10 +1,27 @@
 module BankAccount
   class Account
-    attr_reader :balance
+    #balance: Returns the current account balance [breakfast]
+    attr_reader :id, :balance
 
-    def initialize
-      # (id, initial_balance)
+    # self.new(id, initial_balance): creates a new instance with the instance variable id and 'initial_balance' assigned [breakfast]
+    def initialize(id, initial_balance)
+      # Account cannot be created with initialize negative balance - this will raise an ArgumentError [breakfast]
+      raise ArgumentError, "negative account balance" unless initial_balance >= 0
+
+      @id = id
+      @balance = initial_balance
     end
+
+    # # #withdraw(amount): The input amount gets taken out of the account as result of an ATM transaction
+    #   # return value should be the updated account balance
+    #   # does not allow the account to go negative - Will output a warning message and return the original un-modified balance
+    # def withdraw(amount)
+    # end
+
+    # # #deposit(amount): Adds the input amount to the account balance as a result of an ATM transaction
+    #   # return value should be the updated account balance
+    # def deposit(amount)
+    # end
 
   end # class end
 end # module end
--- spec/lib/account_spec.rb
+++ spec/lib/account_spec.rb
@@ -1,9 +1,89 @@
 describe BankAccount::Account do
 
-  describe 'attr_reader' do
-    it 'responds to #new' do
+  describe '.new(id, initial_balance)' do
+    it 'responds to .new' do
       expect(BankAccount::Account).to respond_to :new
     end
+
+    context 'when instantiating an accepted account' do
+      before :each do
+        @my_account = BankAccount::Account.new(1,125)
+      end
+
+      it 'creates an instance' do
+        expect(@my_account).to be_a BankAccount::Account
+      end
+
+      it 'instance has an id' do
+        expect(@my_account.id).to eq(1)
+      end
+
+      it 'instance has a balance' do
+        expect(@my_account.balance).to eq(125)
+      end
+    end
+
+    context 'when a negative account balance is passed in' do
+      it 'raises an ArgumentError' do
+        expect {BankAccount::Account.new(1, -1)}.to raise_error(ArgumentError)
+      end
+    end
   end
+
+  describe '#balance' do
+    it 'responds' do
+      expect(BankAccount::Account.new(1,125)).to respond_to :balance
+    end
+  end
+
+  # describe '#withdraw(amount)' do
+  #   before :each do
+  #     @my_account = BankAccount::Account.new(1,125)
+  #   end
+
+  #   it 'responds' do
+  #     expect(@my_account).to respond_to :withdraw
+  #   end
+
+  #   it 'updates account balance' do
+  #     @my_account.withdraw(100)
+  #     expect(@my_account.balance).to eq(25)
+  #   end
+
+  #   it 'returns updated account balance' do
+  #     my_account = BankAccount::Account.new(1,125)
+  #     expect(@my_account.withdraw(25)).to eq(100)
+  #   end
+
+  #   context 'when attempting to overdraw' do
+  #     it 'raises an error' do
+  #       expect(@my_account.withdraw(125.5)).to raise_error(ArgumentError)
+  #     end
+
+  #     it 'returns unmodified account balance' do
+  #       expect(@my_account.withdraw(140)).to eq(125)
+  #     end
+  #   end
+  # end
+
+  # describe '#deposit(amount)' do
+  #   before :each do
+  #     @my_account = BankAccount::Account.new(1,125)
+  #   end
+
+  #   it 'responds' do
+  #     expect(@my_account).to respond_to :deposit
+  #   end
+
+  #   it 'updates account balance' do
+  #     @my_account.deposit(100)
+  #     expect(@my_account.balance).to eq(225)
+  #   end
+
+  #   it 'returns updated account balance' do
+  #     my_account = BankAccount::Account.new(1,125)
+  #     expect(@my_account.deposit(25)).to eq(150)
+  #   end
+  # end
 
 end
