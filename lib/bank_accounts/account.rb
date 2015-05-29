ff5db9b9f94e360e4583c7a8788a408f0cf22e52
Lila Garcia <lilagrc@gmail.com>
2015-05-29 09:35:27 -0700

    Added withdraw method and two more specs

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -2,19 +2,25 @@ module BankAccounts
   require "pry"
 
   class Account
-    attr_reader :initial_balance
+    attr_reader :id, :initial_balance
 
     def initialize(id, initial_balance)
       @id = id
 
       if initial_balance < 0
-        raise ArgumentError.new
+        raise ArgumentError.new("You need money to open this account. Please try again")
       else
         @initial_balance = initial_balance
       end
     end
 
-
+    def withdraw(amount)
+      if amount > @initial_balance
+        return "Insufficient funds to remove this amount. Please try a smaller amount."
+      else
+        @initial_balance -= amount
+      end
+    end
 
 
 
--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -6,7 +6,7 @@ describe BankAccounts::Account do
     expect(BankAccounts::Account).to respond_to(:new).with(2).arguments
   end
 
-  it "returns the initial balance" do
+  it "returns the initial balance that was set in initialize" do
     account = BankAccounts::Account.new(4, 100)
     expect(account.initial_balance).to eq(100)
   end
@@ -14,5 +14,21 @@ describe BankAccounts::Account do
   it "raises an ArgumentError if negative number is passed for initial_balance" do
     expect { BankAccounts::Account.new(4, -500) }.to raise_exception(ArgumentError)
   end
+
+  context "When #withdraw is called" do
+    before :each do
+      @account = BankAccounts::Account.new(1, 1000)
+    end
+
+    it "removes the input amount from the balance" do
+      @account.withdraw(500)
+      expect(@account.initial_balance).to eq(500)
+    end
+
+    it "does not allow the account to go negative" do
+      expect(@account.withdraw(1500)).to eq("Insufficient funds to remove this amount. Please try a smaller amount.")
+    end
+  end
+
 
 end #describe
