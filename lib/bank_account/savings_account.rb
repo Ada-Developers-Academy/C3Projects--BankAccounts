6f7e62277d6a2b3ac5ba217ab25ae6a8522d9eaa
Katie Atrops <katrops@gmail.com>
2015-05-29 12:06:34 -0700

    green breakfast tests

--- lib/bank_account/savings_account.rb
+++ lib/bank_account/savings_account.rb
@@ -1,5 +1,5 @@
 module BankAccount
-  class SavingsAccount
+  class SavingsAccount < Account
 
     def initialize(id, initial_balance)
       @id = id
@@ -8,6 +8,26 @@ module BankAccount
       else
         @balance = initial_balance
       end
+    end
+
+    def withdraw(amount)
+      amount += 2
+      if @balance - amount < 10
+        puts "You cannot withdraw #{amount}. It will make your account go below $10."
+      else
+        @balance -= amount
+      end
+      return @balance
+    end
+
+    def add_interest(rate)
+      interest = 0.0
+      interest = @balance * rate/100
+      puts interest
+      @balance += interest
+      puts @balance
+      return interest
+
     end
 
 
--- spec/lib/savings_account_spec.rb
+++ spec/lib/savings_account_spec.rb
@@ -13,7 +13,31 @@ describe BankAccount::SavingsAccount do
       expect{(BankAccount::SavingsAccount.new(3, 5))}.to raise_exception(ArgumentError)
     end
   
+  end
 
+  context "balance" do
+
+    let(:savings_account) {BankAccount::SavingsAccount.new(4, 150)}
+
+    it "withdraws the correct amount + $2" do
+      expect(savings_account.withdraw(50)).to eq 98 
+    end
+
+    it "doesn't let the account go below $10" do
+      expect(savings_account.withdraw(139)).to eq 150
+    end
+
+    it "inherants deposit functionality from account class" do
+      expect(savings_account.deposit(40)).to eq 190
+    end
+
+    it "calculates interest" do
+      expect(savings_account.add_interest(0.25)).to eq 0.375
+    end
+
+    it "returns the correct balance" do
+      expect(savings_account.balance).to eq 150
+    end
   end
 
 end
