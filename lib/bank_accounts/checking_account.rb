6bd58355190c89c1e5f59bdc26f53b098b299ef0
Shanna Chau <shannachau@gmail.com>
2015-05-29 14:05:01 -0700

    Finished .withdraw, .withdraw_using_check, and .reset_checks methods

--- lib/bank_accounts/checking_account.rb
+++ lib/bank_accounts/checking_account.rb
@@ -1,5 +1,50 @@
+require 'pry'
 module BankAccounts
-  class CheckingAccount
+  class CheckingAccount < Account
+    attr_accessor :check_count
+
+    def initialize(id, initial_balance)
+      super
+      @check_count = 0
+    end
+
+    def withdraw(amount)
+      not_num_error unless amount.is_a? Numeric
+      negative_num_error unless amount > 0
+
+      if (amount + 1) > @balance
+        puts ("Insufficient funds. Please withdraw an amount less than or equal to #{@balance}")
+        return @balance
+      else
+        @balance -= (amount + 1)
+      end
+    end
+
+
+    def withdraw_using_check(amount)
+      not_num_error unless amount.is_a? Numeric
+      negative_num_error unless amount > 0
+
+      if amount > (@balance - 10)
+        puts ("Insufficient funds. Please withdraw an amount less than or equal to #{@balance}")
+        return @balance
+      end
+
+      if @check_count > 3
+          @balance -= (amount + 2)
+          @check_count += 1
+      elsif @check_count <= 3
+          @balance -= amount
+          @check_count += 1
+      end
+
+      return @balance
+    end
+
+
+    def reset_checks
+      @check_count = 0
+    end
 
   end
 end
