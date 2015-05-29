8b3d1e5658568a35ccfb018f88960847d6bce720
Anita <asovay@gmail.com>
2015-05-28 18:58:22 -0700

    Added withdraw specs and method for Account class, tests passing.

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -1,11 +1,40 @@
 module BankAccounts
   class Account
 
+    attr_reader :balance
+
     def initialize(id, initial_balance)
       @id = id
-      @initial_balance = initial_balance
+      @balance = initial_balance
+
+      # Raises an error if the initial balance is negative
+      if initial_balance < 0
+        raise ArgumentError.new "Sorry, you can't have a negative balance"
+      end
     end
-   # self.new(id, initial_balance): creates a new instance with the instance variable id and 'initial_balance' assigned
-   # Account cannot be created with initialize negative balance
+
+    def withdraw(amount)
+
+      # Raises warning message if withdraw amount is greater than present balance
+      if amount > @balance
+        raise ArgumentError.new "WARNING you have overdrawn your account. Your current balance is #{@balance}."
+      
+      # Return the updated balance once money is withdrawn from the account
+      else
+        @balance -= amount
+        return @balance
+      end
+    end
+
+   # #self.new(id, initial_balance): creates a new instance with the instance variable id and 'initial_balance' assigned
+    # #Account cannot be created with initialize negative balance - this will raise an ArgumentError (Google this)
+
+   # withdraw(amount): The input amount gets taken out of the account as result of an ATM transaction. Return value should be the updated account balance.
+    # Does not allow the account to go negative - Will output a warning message and return the original un-modified balance
+
+   # deposit(amount): Adds the input amount to the account balance as a result of an ATM transaction. Return value should be the updated account balance
+
+   # balance: Returns the current account balance
+
   end # class
 end # module
--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -2,8 +2,31 @@ require 'spec_helper'
 require 'bank_accounts'
 
 describe BankAccounts::Account do
-  it "responds to class instantiation with 2 arguments" do
-    expect(BankAccounts::Account).to respond_to(:new).with(2).arguments
+
+  context "When creating a Bank account" do
+    it "responds to Account instantiation with 2 arguments" do
+      expect(BankAccounts::Account).to respond_to(:new).with(2).arguments
+    end
+
+    it "raises an error when account is created with a negative balance" do
+      expect{ BankAccounts::Account.new(0, -1) }.to raise_error(ArgumentError, "Sorry, you can't have a negative balance")
+    end
   end
+
+  context "When withdrawing money from account" do
+    let(:account) { BankAccounts::Account.new(0, 200) }
+
+    it "responds to withdraw(amount)" do
+      expect(account).to respond_to(:withdraw)
+    end
+
+    it "returns an updated account balance when withdrawing money" do
+      expect(account.withdraw(10)).to eq(190)
+    end
+
+    it "raises an error when the account is overdrawn" do
+      expect{account.withdraw(500)}.to raise_error(ArgumentError, "WARNING you have overdrawn your account. Your current balance is #{account.balance}.")
+    end
+  end # context
 
 end # describe
