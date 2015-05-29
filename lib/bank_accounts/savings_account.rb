9b84197b958546120580ffd0c1b6b0b269122721
Shanna Chau <shannachau@gmail.com>
2015-05-29 12:02:53 -0700

    Using is_a? Numeric logic for not_num_error

--- lib/bank_accounts/savings_account.rb
+++ lib/bank_accounts/savings_account.rb
@@ -12,6 +12,9 @@ module BankAccounts
     end
 
     def withdraw(amount)
+      not_num_error unless amount.is_a? Numeric
+      negative_num_error unless amount > 0
+
       if (@balance - amount - 2) < 10
         puts "Insufficient funds. Balance cannot be below $10."
         return @balance
@@ -21,6 +24,9 @@ module BankAccounts
     end
 
     def add_interest(rate)
+      not_num_error unless rate.is_a? Numeric
+      negative_num_error unless rate > 0
+
       interest_on_balance = @balance * rate/100
       @balance += interest_on_balance
 
