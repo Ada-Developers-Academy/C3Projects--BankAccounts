680a867f7676feaee3936ca3ba7ca2ec29d62424
Shanna Chau <shannachau@gmail.com>
2015-05-29 09:33:02 -0700

    Added initialize, withdrawl, and deposit methods

--- lib/bank_accounts/account.rb
+++ lib/bank_accounts/account.rb
@@ -1,7 +1,35 @@
+require 'pry'
+
 module BankAccounts
   class Account
-    def self.new(id, initial_balance)
-      
+    attr_accessor :id, :balance
+
+
+    def initialize(id, initial_balance)
+      raise ArgumentError.new("Cannot have negative number for initial balance") if initial_balance < 0
+
+      @id = id
+      @balance = initial_balance
     end
+
+
+    def withdrawl(amount)
+      if amount > @balance
+        puts ("Insufficient funds. Please withdrawl an amount less than or equal to #{@balance}")
+      else
+        @balance -= amount
+      end
+
+      return @balance
+    end
+
+    def deposit(amount)
+      raise ArgumentError.new("Cannot have negative number for initial balance") if amount < 0
+
+      @balance += amount
+    end
+
   end
+
 end
+    # binding.pry
