1bd8c697690d6c65523442ec97b25b61115a71bf
Anita <asovay@gmail.com>
2015-05-29 20:59:00 -0700

    added comments

--- spec/lib/bank_accounts/account_spec.rb
+++ spec/lib/bank_accounts/account_spec.rb
@@ -56,13 +56,3 @@ describe BankAccounts::Account do
   end
 
 end # describe
-
-# Requirements for class Account --------------------------------->
-
-# Create an `Account` class with a minimum of 6 specs. The class should have the following methods:
-# - `self.new(id, initial_balance)`: creates a new instance with the instance variable `id` and 'initial_balance' assigned
-#   - Account cannot be created with initialize negative balance - this will `raise` an `ArgumentError` (Google this)
-# - `#withdraw(amount)`: The input amount gets taken out of the account as result of an ATM transaction. Return value should be the updated account balance.
-#   - Does not allow the account to go negative - Will output a warning message and return the original un-modified balance
-# - `#deposit(amount)`: Adds the input amount to the account balance as a result of an ATM transaction. Return value should be the updated account balance
-# - `#balance`: Returns the current account balance
--- spec/lib/bank_accounts/savings_spec.rb
+++ spec/lib/bank_accounts/savings_spec.rb
@@ -46,17 +46,3 @@ describe BankAccounts::SavingsAccount do
   end
 
 end # describe
-
-# Requirements for class SavingsAccount --------------------------------->
-
-# Create a `SavingsAccount` class with a minimum of 6 specs. The class should inherit behavior from the `Account` class. It should include updated logic within the following methods:
-# - `self.new(id, initial_balance)`: creates a new instance with the instance variable `id` and 'initial_balance' assigned
-#   - The initial balance cannot be less than $10. If it is, this will `raise` an `ArgumentError`
-# - `#withdraw(amount)`: The input amount gets taken out of the account as result of an ATM transaction. Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the balance.
-#   - Does not allow the account to go below the $10 minimum balance - Will output a warning message and return the original un-modified balance
-
-# It should include the following new methods:
-# - `#add_interest(rate)`: Calculate the interest on the balance and add the interest to the balance. Return the interest that was calculated and added to the balance (not the updated balance).
-#   - Input rate is assumed to be a percentage (i.e. 0.25).
-#   - The formula for calculating interest is `balance * rate/100`
-#     - Example: If the interest rate is 0.25% and the balance is $10,000, then the interest is $25 and the new balance becomes $10,025.
